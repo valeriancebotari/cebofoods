213bb4433fa8bebc49a9beb5b49372db3fb803a5
vali cebo <cebotari.valerian@gmail.com>
2023-07-19 14:44:23 +0300

    adding index.html style.css and script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+// MENU
+const divtoShow = 'nav .menu';
+const divPopup = document.querySelector(divtoShow);
+const divTrigger = document.querySelector('.m-trigger');
+
+divTrigger.addEventListener('click', () => {
+    setTimeout( () => {
+        if (!divPopup.classList.contains('show')) {
+            divPopup.classList.add('show');
+            document.body.classList.add('menu-visible');
+        }
+    }, 250); // end setTimeout
+});
+
+
+// automatically close by clicking outside of menu
+document.addEventListener('click', (e) => {
+   const isClosest = e.target.closest(divtoShow);
+   
+   if (!isClosest && divPopup.classList.contains('show')) {
+       divPopup.classList.remove('show');
+       document.body.classList.remove('menu-visible');  
+   }
+});
+
+// Search
+const sTrigger = document.querySelector('.s-trigger');
+const addclass = document.querySelector('.site');
+sTrigger.addEventListener('click', () => {
+    addclass.classList.toggle('showsearch');
+});
+
+// Slider 
+const sliderThumb = new Swiper('.thumb-nav', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    slidesPerGroup: false,
+    breakpoints: {
+        992: {
+            direction: 'vertical'
+        }
+    }
+});
+
+const theSlider = new Swiper('.thumb-big', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    thumbs: {
+        swiper: sliderThumb,
+    }
+});
